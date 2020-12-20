f556776d4baf493777d30e4025f18db794f98594
sworsman31415926 <hmf31415926@126.com>
2020-12-20 16:00:23 +0800

    add the first code

---
+++ CMakeFiles/test_01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_01.dir/hello.cpp.o"
+  "libtest_01.dll.a"
+  "test_01.exe"
+  "test_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
